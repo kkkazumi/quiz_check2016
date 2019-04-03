f796d91d89e4441dcb96542796f1ade4668dc8bc
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2019-04-03 20:41:10 +0900

    finished learning

--- main.py
+++ main.py
@@ -1,36 +1,45 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation
+from keras.layers.advanced_activations import LeakyReLU
+import numpy as np
 
 INPUT_DIM = 10
 MID_UNIT = 15
 OUTPUT_DIM = 1
-EPOCH = 2000
+EPOCH = 5000
 
-username = [inusan, kubosan, kumasan, nekosan, sarada, test119, test120, test121, tomato, torisan, usagisan]
+username = ['inusan', 'kubosan', 'kumasan', 'nekosan', 'sarada', 'test119', 'test120', 'test121', 'tomato', 'torisan', 'usagisan']
 
 #define NN framework
 model = Sequential()
-model.add(Dense(MID_UNIT, input_dim = INPUT_DIM, activation='sigmoid'))
-model.add(Dense(OUTPUT_DIM, activation='softmax'))
-
-model.compile(optimizer='Adam', loss='categorical_crossentropy')
+model.add(Dense(MID_UNIT, input_dim = INPUT_DIM, activation='relu'))
+model.add(Dense(OUTPUT_DIM, activation='relu'))
+model.compile(optimizer='Adam', loss='mean_squared_error')
 
 for i_name in username:
-	#set input, output data to train
-	xdata = "~/data/jiken/" + i_name + "/factor_before.csv"
-	ydata = "~/data/jiken/" + i_name + "/kibun_before.csv"
-	x_train = np.loadtxt(xdata,delimiter=',')
-	y_train = np.loadtxt(ydata,delimiter=',')
 
-	#train
-	model.fit(x=x_train, y=y_train, nb_epoch=EPOCH)
+	#set input, output data to train
+	xdata = "./jiken/" + i_name + "/factor_before.csv"
+	ydata = "./jiken/" + i_name + "/kibun_before.csv"
+	x_train = np.loadtxt(xdata,delimiter='\t')
+	y_train = np.loadtxt(ydata,delimiter='\t')
+
+	train=model.fit(x=x_train, y=y_train, nb_epoch=EPOCH)
 
 	#set data to test
-	xtest = "~/data/jiken/" + i_name + "/factor_after.csv"
-	yans = "~/data/jiken/" + i_name + "/kibun_after.csv"
+	xtest = np.loadtxt("./jiken/" + i_name + "/factor_after.csv",delimiter='\t')
+	yans = np.loadtxt("./jiken/" + i_name + "/kibun_after.csv",delimiter='\t')
+
+	ycheck = model.predict(x_train)
+	print("ch-factor","ch-predicted","ch-self-assessed")
+	print(ycheck[:,0], y_train)
+	print(np.mean(ycheck[:,0]-y_train))
 
 	#test output
 	ytest = model.predict(xtest)
-
 	print("factor","predicted","self-assessed")
-	print(x_test, y_test, yans)
+	print(xtest, ytest, yans)
+	outname = "./jiken/" + i_name + "/kibun_predicted50k.csv"
+	np.savetxt(outname,ytest[:,0])
+	lossname = "./jiken/" + i_name + "/loss_history50k.csv"
+	np.savetxt(lossname,train.history['loss'])
