81cb60ce1ab09e61cd5b738c705c2edd2e86b255
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2019-04-18 01:31:27 +0900

    changed bit for nn prediction

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from keras.layers import Dense, Activation
 from keras.layers.advanced_activations import LeakyReLU
 import numpy as np
 
-INPUT_DIM = 10
+INPUT_DIM = 11
 MID_UNIT = 15
-OUTPUT_DIM = 1
+OUTPUT_DIM = 4
 EPOCH = 5000
 
 username = ['inusan', 'kubosan', 'kumasan', 'nekosan', 'sarada', 'test119', 'test120', 'test121', 'tomato', 'torisan', 'usagisan']
@@ -20,15 +20,22 @@ for i_name in username:
 
 	#set input, output data to train
 	xdata = "./jiken/" + i_name + "/factor_before.csv"
-	ydata = "./jiken/" + i_name + "/kibun_before.csv"
-	x_train = np.loadtxt(xdata,delimiter='\t')
+	x2data = "./jiken/" + i_name + "/kibun_before.csv"
+	ydata = "./jiken/" + i_name + "/signal_before.csv"
+	x1_train = np.loadtxt(xdata,delimiter='\t')
+	x2_train = np.loadtxt(x2data,delimiter='\t')
+	x_train = np.hstack((x1_train,x2_train))
+
 	y_train = np.loadtxt(ydata,delimiter='\t')
 
 	train=model.fit(x=x_train, y=y_train, nb_epoch=EPOCH)
 
 	#set data to test
-	xtest = np.loadtxt("./jiken/" + i_name + "/factor_after.csv",delimiter='\t')
-	yans = np.loadtxt("./jiken/" + i_name + "/kibun_after.csv",delimiter='\t')
+	for i in range():
+	x1test = np.loadtxt("./jiken/" + i_name + "/factor_after.csv",delimiter='\t')
+	x2test = np.loadtxt("./jiken/" + i_name + "/kibun_after.csv",delimiter='\t')
+	xtest = np.hstack((x1test,x2test))
+	yans = np.loadtxt("./jiken/" + i_name + "/signal_after.csv",delimiter='\t')
 
 	ycheck = model.predict(x_train)
 	print("ch-factor","ch-predicted","ch-self-assessed")
@@ -39,7 +46,7 @@ for i_name in username:
 	ytest = model.predict(xtest)
 	print("factor","predicted","self-assessed")
 	print(xtest, ytest, yans)
-	outname = "./jiken/" + i_name + "/kibun_predicted50k.csv"
+	outname = "./jiken/" + i_name + "/kibun_pre.csv"
 	np.savetxt(outname,ytest[:,0])
-	lossname = "./jiken/" + i_name + "/loss_history50k.csv"
+	lossname = "./jiken/" + i_name + "/loss_hist.csv"
 	np.savetxt(lossname,train.history['loss'])
--- plot.py
+++ plot.py
@@ -1,13 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-username = ['inusan', 'kubosan', 'kumasan', 'nekosan', 'sarada', 'test119', 'test120', 'test121', 'tomato', 'torisan', 'usagisan']
+username = ['inusan', 'kumasan', 'nekosan', 'test119', 'test120', 'test121', 'tomato', 'torisan', 'usagisan']
+#username = ['inusan', 'kubosan', 'kumasan', 'nekosan', 'sarada', 'test119', 'test120', 'test121', 'tomato', 'torisan', 'usagisan']
 
 for name in username:
-  predicted = np.loadtxt("./jiken/"+name+"/kibun_predicted.csv",delimiter=",")
+  predicted = np.loadtxt("./jiken/"+name+"/m_pred.csv",delimiter=",")
   answered = np.loadtxt("./jiken/"+name+"/kibun_after.csv",delimiter=",")
   x = range(10)
+  fig, ax1 = plt.subplots()
+  ln1 = ax1.plot(x,predicted,'C0',label="predicted")
+  ax2 = ax1.twinx()
+  ln2 = ax2.plot(x,answered,'C1',label="answered")
+
+  h1, l1 = ax1.get_legend_handles_labels()
+  h2, l2 = ax2.get_legend_handles_labels()
+  ax1.legend(h1+h2,l1+l2,loc="lower right")
+
+  ax1.set_xlabel('time transition')
+  ax1.set_ylabel('predicted M')
+  ax2.set_ylabel('answered M')
   plt.title(name)
-  plt.plot(x,predicted,label="predicted")
-  plt.plot(x,answered,label="answered")
+
   plt.show()
