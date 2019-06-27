3ffe02ffbd81ff281196b7282c379bd6ae6bf8cc
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2019-06-27 13:58:56 +0900

    add module to put id numbe

--- plot_bar.py
+++ plot_bar.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from conv_num import * #for id number
 
 username = ['inusan', 'kumasan', 'nekosan', 'test119', 'test120', 'test121', 'tomato', 'torisan', 'usagisan']
 #username = ['inusan', 'kubosan', 'kumasan', 'nekosan', 'sarada', 'test119', 'test120', 'test121', 'tomato', 'torisan', 'usagisan']
@@ -15,9 +16,9 @@ for name in username:
   answered = np.loadtxt("./jiken/"+name+"/kibun_after.csv",delimiter=",")
   faced = np.loadtxt("./jiken/"+name+"/signal_after.csv",delimiter="\t")
   before_face = np.loadtxt("./jiken/"+name+"/signal_before.csv",delimiter="\t")
-  plt.plot(before_face[:,0])
-  plt.title(name)
-  plt.show()
+  #plt.plot(before_face[:,0])
+  #plt.title(name)
+  #plt.show()
 
   corr_ms[i] = np.corrcoef(answered,faced[:,0])[1,0]
   corr_mm[i] = np.corrcoef(answered,predicted)[1,0]
@@ -31,11 +32,13 @@ for name in username:
 left = np.arange(len(username))
 width = 0.3
 
-#g1=plt.bar(left,corr_ms,color='r',label='ms',width=width,align='center')
-#g2=plt.bar(left+width, corr_mm, color='b',label='mm', tick_label=username,width=width, align='center')
+name_list = conv_list(username)
+
+g1=plt.bar(left,corr_ms,color='r',label='observed happy face',width=width,align='center')
+g2=plt.bar(left+width, corr_mm, color='b',label='estimated mental state', tick_label=name_list,width=width, align='center')
 #g3=plt.bar(left+width+width, var, color='g',label='var', tick_label=username,width=width, align='center')
-plt.bar(left+width+width, var, color='g',label='var', tick_label=username,width=width, align='center')
-#plt.legend(handles=[g1,g2,g3],loc='best',shadow=True)
+#plt.bar(left+width+width, var, color='g',label='var', tick_label=username,width=width, align='center')
+plt.legend(handles=[g1,g2],loc='best',shadow=True)
 plt.xlabel('user id')
 plt.ylabel('correlation of answered M vs face/predicted M')
 
