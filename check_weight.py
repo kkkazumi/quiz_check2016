376f2af403166e1fa47262479b1d89a4bd28b126
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2019-12-06 00:20:22 +0900

    add file to check weights

---
+++ check_weight.py
@@ -0,0 +1,73 @@
+import matplotlib
+import numpy as np
+from func import *
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from conv_num import *
+
+DAT_NUM = 50
+EMO_NUM = 4
+SIT_NUM = 10
+
+name_list = ["inusan","kumasan","nekosan","test119","test120","test121","tomato","torisan","usagisan"]
+#name_list = ["inusan","kubosan","kumasan","nekosan","test119","test120","test121","tomato","torisan","usagisan"]
+#name_list = ["inusan","kubosan","kumasan","nekosan","test119","test120","test121","tomato","torisan","usagisan","sarada"]
+
+xlabel = ["hap","sup","ang","sad"]
+class SqueezedNorm(matplotlib.colors.Normalize):
+    def __init__(self, vmin=None, vmax=None, mid=0, s1=2, s2=2, clip=False):
+        self.vmin = vmin # minimum value
+        self.mid  = mid  # middle value
+        self.vmax = vmax # maximum value
+        self.s1=s1; self.s2=s2
+        f = lambda x, zero,vmax,s: np.abs((x-zero)/(vmax-zero))**(1./s)*0.5
+        self.g = lambda x, zero,vmin,vmax, s1,s2: f(x,zero,vmax,s1)*(x>=zero) - \
+                                             f(x,zero,vmin,s2)*(x<zero)+0.5
+        matplotlib.colors.Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        r = self.g(value, self.mid,self.vmin,self.vmax, self.s1,self.s2)
+        return np.ma.masked_array(r)
+
+
+
+func_num = [[0,1,2,3],[4,5,6,7],[8,9,10,11],
+  [12,13,14,15],[16,17,18,19],[20,21,22,23],
+  [24,25,26,27],[28,29,30,31],[32,33,34,35],[36,37,38,39]]
+
+
+if __name__ == '__main__':
+
+  fig = plt.figure()
+  j=0
+
+  for i_name in name_list:
+    #plt.subplot(3,4,j+1)
+    corr_out = np.zeros((SIT_NUM,4))
+    for num in range(30):
+      plt.subplots_adjust(wspace=0.4,hspace=0.6)
+      hap_w= np.loadtxt("./jrm_test/5th_test/"+str(conv_num(i_name))+"/hap_weight40-"+str(num)+".csv",delimiter="\t")
+      sup_w= np.loadtxt("./jrm_test/5th_test/"+str(conv_num(i_name))+"/sup_weight40-"+str(num)+".csv",delimiter="\t")
+      ang_w= np.loadtxt("./jrm_test/5th_test/"+str(conv_num(i_name))+"/ang_weight40-"+str(num)+".csv",delimiter="\t")
+      sad_w= np.loadtxt("./jrm_test/5th_test/"+str(conv_num(i_name))+"/sad_weight40-"+str(num)+".csv",delimiter="\t")
+
+      corr_out[:,0]=hap_w
+      corr_out[:,1]=sup_w
+      corr_out[:,2]=ang_w
+      corr_out[:,3]=sad_w
+
+      sns.heatmap(abs(corr_out),cmap="seismic",norm=SqueezedNorm(vmin=-1, vmax=1, mid=0),cbar=False)
+      #plt.plot(range(10),hap_w)
+      plt.xticks([0.5,1.5,2.5,3.5],xlabel)
+      j+=1
+      plt.title(i_name)
+      #print np.mean(abs(corr_out_all),axis=0)
+      #print conv_num(i_name),float(np.count_nonzero(corr_out>0.5))/float(corr_out.size)
+      plt.pause(.1)
+    plt.cla()
+
+#np.savetxt("phi_corr_"+i_name+".csv",corr_out,delimiter=",")
+#output of phi function
+#sprintf(out_name,"./jiken/%s/phi_out.csv",argv[1]);
+#sprintf(phi_data,"./jiken/%s/phi_stdata.csv",argv[1]);//statistical data
