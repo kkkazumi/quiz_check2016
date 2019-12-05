1dc85047174944a6fc4f3f6822a59eaba8666c50
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2019-12-05 22:30:22 +0900

    changed so that 50 data is used to check basis function's acuracy

--- out_new.py
+++ out_new.py
@@ -4,11 +4,14 @@ from func import *
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-DAT_NUM = 10
+from conv_num import *
+
+DAT_NUM = 50
 EMO_NUM = 4
 SIT_NUM = 10
 
-name_list = ["inusan","kubosan","kumasan","nekosan","test119","test120","test121","tomato","torisan","usagisan"]
+name_list = ["inusan","kumasan","nekosan","test119","test120","test121","tomato","torisan","usagisan"]
+#name_list = ["inusan","kubosan","kumasan","nekosan","test119","test120","test121","tomato","torisan","usagisan"]
 #name_list = ["inusan","kubosan","kumasan","nekosan","test119","test120","test121","tomato","torisan","usagisan","sarada"]
 
 xlabel = ["hap","sup","ang","sad"]
@@ -34,7 +37,7 @@ func_num = [[0,1,2,3],[4,5,6,7],[8,9,10,11],
   [24,25,26,27],[28,29,30,31],[32,33,34,35],[36,37,38,39]]
 
 def phi(factor,kibun,emo_num):
-  ret = np.zeros(10)
+  ret = np.zeros(SIT_NUM)
 
   for sit_num in range(SIT_NUM):
     ret[sit_num] = func(factor[sit_num],kibun,func_num[sit_num][emo_num])
@@ -66,14 +69,26 @@ if __name__ == '__main__':
 
   j=0
 
+  corr_out_all = np.zeros((len(name_list),SIT_NUM,4))
+
   for i_name in name_list:
     plt.subplot(3,4,j+1)
-    factor = np.loadtxt("./jiken/"+i_name+"/factor_after.csv",delimiter="\t")
-    kibun = np.loadtxt("./jiken/"+i_name+"/kibun_after.csv",delimiter="\t")
-    signal = np.loadtxt("./jiken/"+i_name+"/signal_after.csv",delimiter="\t")
+    factor1 = np.loadtxt("./jiken/"+i_name+"/factor_after.csv",delimiter="\t")
+    factor2 = np.loadtxt("./jiken/"+i_name+"/factor_after2.csv",delimiter="\t")
+    factor3 = np.loadtxt("./jiken/"+i_name+"/factor_before.csv",delimiter="\t")
+    factor=np.vstack((factor1,factor2,factor3))
+    kibun1 = np.loadtxt("./jiken/"+i_name+"/kibun_after.csv",delimiter="\t")
+    kibun2 = np.loadtxt("./jiken/"+i_name+"/kibun_after2.csv",delimiter="\t")
+    kibun3 = np.loadtxt("./jiken/"+i_name+"/kibun_before.csv",delimiter="\t")
+    _kibun=np.append(kibun1,kibun2)
+    kibun=np.append(_kibun,kibun3)
+    signal1 = np.loadtxt("./jiken/"+i_name+"/signal_after.csv",delimiter="\t")
+    signal2 = np.loadtxt("./jiken/"+i_name+"/signal_after2.csv",delimiter="\t")
+    signal3 = np.loadtxt("./jiken/"+i_name+"/signal_before.csv",delimiter="\t")
+    signal=np.vstack((signal1,signal2,signal3))
 
     phi_out_arr = phi_out(factor,kibun)
-    corr_out = np.zeros((10,4))
+    corr_out = np.zeros((SIT_NUM,4))
     
     for sit_num in range(SIT_NUM):
       #array = signal - phi_out[:,sit_num,:]
@@ -81,10 +96,15 @@ if __name__ == '__main__':
         corr = np.corrcoef(signal[:,emo_num],phi_out_arr[:,sit_num,emo_num])
         corr_out[sit_num,emo_num]=corr[0,1]
 
+    corr_out_all[j,:]=corr_out
+
     sns.heatmap(corr_out,cmap="seismic",norm=SqueezedNorm(vmin=-1, vmax=1, mid=0))
     plt.xticks([0.5,1.5,2.5,3.5],xlabel)
     j+=1
     plt.title(i_name)
+    #print np.mean(abs(corr_out_all),axis=0)
+    #print conv_num(i_name),float(np.count_nonzero(corr_out>0.5))/float(corr_out.size)
+    print conv_num(i_name),np.count_nonzero(corr_out>0.1)
   plt.show()
 
 #np.savetxt("phi_corr_"+i_name+".csv",corr_out,delimiter=",")
