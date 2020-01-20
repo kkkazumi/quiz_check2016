7bd77e6c9e1d8bd92d3da4066f71f84c9d5d0bec
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2020-01-20 20:03:49 +0900

    add optuna for trial

--- 40_nntest.py
+++ 40_nntest.py
@@ -1,5 +1,8 @@
-from keras.models import Sequential
-from keras.layers import Dense, Activation
+import optuna
+
+import keras.backend as K
+from keras.models import Sequential, Model
+from keras.layers import Input, Dense, Activation
 from keras.layers.advanced_activations import LeakyReLU
 import numpy as np
 import scipy as sp
@@ -10,15 +13,56 @@ MID_UNIT = 15
 OUTPUT_DIM = 4
 EPOCH = 50000
 
-#define NN framework
-model = Sequential()
-model.add(Dense(MID_UNIT, input_dim = INPUT_DIM, activation='relu'))
-model.add(Dense(OUTPUT_DIM, activation='relu'))
-model.compile(optimizer='Adam', loss='mean_squared_error')
 
-#for name_num in (3,4,5,6,7,8,9):
+def create_model(num_layer, activation, mid_units, num_filters):
+  """
+  num_layer : 畳込み層の数
+  activation : 活性化関数
+  mid_units : FC層のユニット数
+  num_filters : 各畳込み層のフィルタ数
+  """
+  inputs = Input(shape=(INPUT_DIM,))
+  x = Convolution2D(filters=num_filters[0], kernel_size=(3,3), padding="same", activation=activation)(inputs)
+  for i in range(1,num_layer):
+      x = Convolution2D(filters=num_filters[i], kernel_size=(3,3), padding="same", activation=activation)(x)
+
+  x = GlobalAveragePooling2D()(x)
+  x = Dense(units=mid_units, activation=activation)(x)
+  x = Dense(units=10, activation="softmax")(x)
+
+  model = Model(inputs=inputs, outputs=x)
+  return model
+
+def objective(trial):
+  #セッションのクリア
+  K.clear_session()
+
+  #最適化するパラメータの設定
+  #中間層のユニット数
+  mid_units = int(trial.suggest_discrete_uniform("mid_units", 10, 20, 30))
+
+  #活性化関数
+  activation = trial.suggest_categorical("activation", ["relu", "sigmoid", "tanh"])
+
+  #optimizer
+  optimizer = trial.suggest_categorical("optimizer", ["sgd", "adam", "rmsprop"])
+
+  #define NN framework
+  model = Sequential()
+  model.add(Dense(mid_units, input_dim = INPUT_DIM, activation=activation))
+  model.add(Dense(OUTPUT_DIM, activation='relu'))
+
+  model.compile(optimizer=optimizer,
+        loss="mean_squared_error",
+        metrics=["accuracy"])
+
+  #検証用データに対する正答率が最大となるハイパーパラメータを求める
+
+study = optuna.create_study()
+study.optimize(objective, n_trial=100)
+
+
 for name_num in (0,1,2,3,4,5,6,7,8):
-#name_num = 2
   dir_name = "./jrm_test/" + str(name_num+1)
   for set_num in (5,10,15,20,25,30,35,40):
 
@@ -44,8 +88,6 @@ for name_num in (0,1,2,3,4,5,6,7,8):
       factor_test = np.loadtxt(dir_name + "/factor_test" + i_csv ,delimiter=',')
       mood_test = np.loadtxt(dir_name + "/mood_test" + i_csv ,delimiter=',')
       face_test = np.loadtxt(dir_name + "/face_test" + i_csv ,delimiter=',')
-      #m_ans = np.loadtxt("./jiken/" + i_name + "/kibun_after.csv",delimiter='\t')
-      #y_sig = np.loadtxt("./jiken/" + i_name + "/signal_after.csv",delimiter='\t')
 
       m_pred = np.zeros_like(mood_test)
 
@@ -67,8 +109,8 @@ for name_num in (0,1,2,3,4,5,6,7,8):
 
       #test output
       outname = dir_name + "/estimated_nn" + i_csv
-      np.savetxt(outname,m_pred,fmt="%.3f")
+      #np.savetxt(outname,m_pred,fmt="%.3f")
       lossname = dir_name + "/nn_loss" + i_csv
-      np.savetxt(lossname,train.history['loss'])
-    np.savetxt(dir_name+"/nn_corr-"+str(set_num)+".csv",r,fmt='%.5f',delimiter=',')
-    np.savetxt(dir_name+"/nn_p-"+str(set_num)+".csv",p,fmt='%.5f',delimiter=',')
+      #np.savetxt(lossname,train.history['loss'])
+    #np.savetxt(dir_name+"/nn_corr-"+str(set_num)+".csv",r,fmt='%.5f',delimiter=',')
+    #np.savetxt(dir_name+"/nn_p-"+str(set_num)+".csv",p,fmt='%.5f',delimiter=',')
