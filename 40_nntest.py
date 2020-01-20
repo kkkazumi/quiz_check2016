a1c05ba415c1a050f0f081a752a9de8356c8a99f
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2020-01-20 21:32:10 +0900

    add hi parameter optimizer

--- 40_nntest.py
+++ 40_nntest.py
@@ -13,55 +13,6 @@ MID_UNIT = 15
 OUTPUT_DIM = 4
 EPOCH = 50000
 
-
-def create_model(num_layer, activation, mid_units, num_filters):
-  """
-  num_layer : 畳込み層の数
-  activation : 活性化関数
-  mid_units : FC層のユニット数
-  num_filters : 各畳込み層のフィルタ数
-  """
-  inputs = Input(shape=(INPUT_DIM,))
-  x = Convolution2D(filters=num_filters[0], kernel_size=(3,3), padding="same", activation=activation)(inputs)
-  for i in range(1,num_layer):
-      x = Convolution2D(filters=num_filters[i], kernel_size=(3,3), padding="same", activation=activation)(x)
-
-  x = GlobalAveragePooling2D()(x)
-  x = Dense(units=mid_units, activation=activation)(x)
-  x = Dense(units=10, activation="softmax")(x)
-
-  model = Model(inputs=inputs, outputs=x)
-  return model
-
-def objective(trial):
-  #セッションのクリア
-  K.clear_session()
-
-  #最適化するパラメータの設定
-  #中間層のユニット数
-  mid_units = int(trial.suggest_discrete_uniform("mid_units", 10, 20, 30))
-
-  #活性化関数
-  activation = trial.suggest_categorical("activation", ["relu", "sigmoid", "tanh"])
-
-  #optimizer
-  optimizer = trial.suggest_categorical("optimizer", ["sgd", "adam", "rmsprop"])
-
-  #define NN framework
-  model = Sequential()
-  model.add(Dense(mid_units, input_dim = INPUT_DIM, activation=activation))
-  model.add(Dense(OUTPUT_DIM, activation='relu'))
-
-  model.compile(optimizer=optimizer,
-        loss="mean_squared_error",
-        metrics=["accuracy"])
-
-  #検証用データに対する正答率が最大となるハイパーパラメータを求める
-
-study = optuna.create_study()
-study.optimize(objective, n_trial=100)
-
-
 for name_num in (0,1,2,3,4,5,6,7,8):
   dir_name = "./jrm_test/" + str(name_num+1)
   for set_num in (5,10,15,20,25,30,35,40):
@@ -81,6 +32,48 @@ for name_num in (0,1,2,3,4,5,6,7,8):
       x_train = np.hstack((x1_train,x2_train.reshape(-1,1)))
 
       y_train = np.loadtxt(face_train,delimiter=',')
+
+      def objective(trial):
+        #セッションのクリア
+        K.clear_session()
+
+        #最適化するパラメータの設定
+        #中間層のユニット数
+        mid_units = trial.suggest_int("mid_units", 5, 100)
+
+        #活性化関数
+        activation = trial.suggest_categorical("activation", ["relu", "sigmoid", "tanh"])
+
+        #optimizer
+        optimizer = trial.suggest_categorical("optimizer", ["sgd", "adam", "rmsprop"])
+
+        #define NN framework
+        model = Sequential()
+        model.add(Dense(mid_units, input_dim = INPUT_DIM, activation=activation))
+        model.add(Dense(OUTPUT_DIM, activation=activation))
+
+        model.compile(optimizer=optimizer,
+              loss="mean_squared_error",
+              metrics=["accuracy"])
+
+        history = model.fit(x_train, y_train, verbose=0, epochs=200, batch_size=128, validation_split=0.1)
+        return 1 - history.history["val_acc"][-1]
+
+      study = optuna.create_study()
+      study.optimize(objective, n_trials=100)
+
+      mid_units=study.best_params["mid_units"]
+      activation=study.best_params["activation"]
+      optimizer=study.best_params["optimizer"]
+
+      #define NN framework
+      model = Sequential()
+      model.add(Dense(mid_units, input_dim = INPUT_DIM, activation=activation))
+      model.add(Dense(OUTPUT_DIM, activation=activation))
+
+      model.compile(optimizer=optimizer,
+            loss="mean_squared_error",
+            metrics=["accuracy"])
 
       train=model.fit(x=x_train, y=y_train, nb_epoch=EPOCH)
 
@@ -109,8 +102,8 @@ for name_num in (0,1,2,3,4,5,6,7,8):
 
       #test output
       outname = dir_name + "/estimated_nn" + i_csv
-      #np.savetxt(outname,m_pred,fmt="%.3f")
+      np.savetxt(outname,m_pred,fmt="%.3f")
       lossname = dir_name + "/nn_loss" + i_csv
-      #np.savetxt(lossname,train.history['loss'])
-    #np.savetxt(dir_name+"/nn_corr-"+str(set_num)+".csv",r,fmt='%.5f',delimiter=',')
-    #np.savetxt(dir_name+"/nn_p-"+str(set_num)+".csv",p,fmt='%.5f',delimiter=',')
+      np.savetxt(lossname,train.history['loss'])
+    np.savetxt(dir_name+"/nn_corr-"+str(set_num)+".csv",r,fmt='%.5f',delimiter=',')
+    np.savetxt(dir_name+"/nn_p-"+str(set_num)+".csv",p,fmt='%.5f',delimiter=',')
