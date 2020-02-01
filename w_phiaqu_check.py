f61764aeba41287abe5656d6d73ef6dafa295205
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2020-02-01 23:11:14 +0900

    file path changed

--- w_phiaqu_check.py
+++ w_phiaqu_check.py
@@ -1,14 +1,14 @@
 import matplotlib
 import numpy as np
-from func import *
+from func_new import *
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 from conv_num import *
 
-DAT_NUM = 50
 EMO_NUM = 4
 SIT_NUM = 10
+USR_NUM = 9
 
 name_list = ["inusan","kumasan","nekosan","test119","test120","test121","tomato","torisan","usagisan"]
 #name_list = ["inusan","kubosan","kumasan","nekosan","test119","test120","test121","tomato","torisan","usagisan"]
@@ -44,7 +44,8 @@ def phi(factor,kibun,emo_num):
   ret = np.zeros(SIT_NUM)
 
   for sit_num in range(SIT_NUM):
-    ret[sit_num] = func(factor[sit_num],kibun,func_num[sit_num][emo_num])
+    #func(factor[sit_num],kibun,func_num[sit_num][emo_num])
+    ret[sit_num] = func(inv_norm(func_num[sit_num][emo_num],factor[sit_num]/100.0),kibun*10.0+0.001,func_num[sit_num][emo_num])
 
   return ret
 
@@ -69,27 +70,17 @@ def phi_out(factor,kibun):
 
 if __name__ == '__main__':
   fig = plt.figure()
-  plt.subplots_adjust(wspace=0.4,hspace=0.6)
 
   j=0
 
   corr_out_all = np.zeros((len(name_list),SIT_NUM,4))
+  sim = np.zeros((USR_NUM,EMO_NUM))
 
-  for i_name in name_list:
-    plt.subplot(3,4,j+1)
-    factor1 = np.loadtxt("./jiken/"+i_name+"/factor_after.csv",delimiter="\t")
-    factor2 = np.loadtxt("./jiken/"+i_name+"/factor_after2.csv",delimiter="\t")
-    factor3 = np.loadtxt("./jiken/"+i_name+"/factor_before.csv",delimiter="\t")
-    factor=np.vstack((factor1,factor2,factor3))
-    kibun1 = np.loadtxt("./jiken/"+i_name+"/kibun_after.csv",delimiter="\t")
-    kibun2 = np.loadtxt("./jiken/"+i_name+"/kibun_after2.csv",delimiter="\t")
-    kibun3 = np.loadtxt("./jiken/"+i_name+"/kibun_before.csv",delimiter="\t")
-    _kibun=np.append(kibun1,kibun2)
-    kibun=np.append(_kibun,kibun3)
-    signal1 = np.loadtxt("./jiken/"+i_name+"/signal_after.csv",delimiter="\t")
-    signal2 = np.loadtxt("./jiken/"+i_name+"/signal_after2.csv",delimiter="\t")
-    signal3 = np.loadtxt("./jiken/"+i_name+"/signal_before.csv",delimiter="\t")
-    signal=np.vstack((signal1,signal2,signal3))
+  for i_name in range(USR_NUM):
+
+    factor = np.loadtxt("/home/kazumi/prog/quiz_check2016/jrm_test/"+str(i_name+1)+"/factor_train50-0.csv",delimiter=",")
+    kibun = np.loadtxt("/home/kazumi/prog/quiz_check2016/jrm_test/"+str(i_name+1)+"/mood_train50-0.csv",delimiter=",")
+    signal = np.loadtxt("/home/kazumi/prog/quiz_check2016/jrm_test/"+str(i_name+1)+"/face_train50-0.csv",delimiter=",")
 
     phi_out_arr = phi_out(factor,kibun)
     corr_out = np.zeros((SIT_NUM,4))
@@ -102,15 +93,15 @@ if __name__ == '__main__':
         corr_out[sit_num,emo_num]=corr[0,1]
 
     corr_out_all[j,:]=corr_out
-    sim = np.zeros((1,4))
 
     num=0
 
+    #for num in range(30):
     plt.subplots_adjust(wspace=0.4,hspace=0.6)
-    hap_w= np.loadtxt("./jrm_test/"+str(conv_num(i_name))+"/hap_weight50-"+str(num)+".csv",delimiter="\t")
-    sup_w= np.loadtxt("./jrm_test/"+str(conv_num(i_name))+"/sup_weight50-"+str(num)+".csv",delimiter="\t")
-    ang_w= np.loadtxt("./jrm_test/"+str(conv_num(i_name))+"/ang_weight50-"+str(num)+".csv",delimiter="\t")
-    sad_w= np.loadtxt("./jrm_test/"+str(conv_num(i_name))+"/sad_weight50-"+str(num)+".csv",delimiter="\t")
+    hap_w= np.loadtxt("./jrm_test/"+str(i_name+1)+"/hap_weight50-"+str(num)+".csv",delimiter="\t")
+    sup_w= np.loadtxt("./jrm_test/"+str(i_name+1)+"/sup_weight50-"+str(num)+".csv",delimiter="\t")
+    ang_w= np.loadtxt("./jrm_test/"+str(i_name+1)+"/ang_weight50-"+str(num)+".csv",delimiter="\t")
+    sad_w= np.loadtxt("./jrm_test/"+str(i_name+1)+"/sad_weight50-"+str(num)+".csv",delimiter="\t")
 
     weight_out[:,0]=hap_w
     weight_out[:,1]=sup_w
@@ -120,23 +111,27 @@ if __name__ == '__main__':
 
     for i in range(EMO_NUM):
       #sim[num,i]=cos_sim(corr_out[:,i], weight_out[:,i])
-      sim[num,i]=np.corrcoef(corr_out[:,i], weight_out[:,i])[0,1]
+      #corr_out[:,i]
+      #print("corr_out[:,",i,"]",corr_out[:,i])
+      #weight_out[:,i]
+      sim[i_name,i]=np.corrcoef(corr_out[:,i], weight_out[:,i])[0,1]
+    sim2 = np.corrcoef(corr_out.flatten(),weight_out.flatten())[0,1]
 
-    print sim[0,0],",", sim[0,1],",", sim[0,2],",", sim[0,3]
-    #print np.mean((sim),axis=0)
-    #print np.count_nonzero(sim>0.1), sim.size
+
+    print i_name,sim2, ":",sim[i_name,0],",", sim[i_name,1],",", sim[i_name,2],",", sim[i_name,3]
 
 
-    sns.heatmap(corr_out,cmap="seismic",norm=SqueezedNorm(vmin=-1, vmax=1, mid=0))
-    plt.xticks([0.5,1.5,2.5,3.5],xlabel)
+    ###sns.heatmap(corr_out,cmap="seismic",norm=SqueezedNorm(vmin=-1, vmax=1, mid=0))
+
     j+=1
-    plt.title(i_name)
-    #print np.mean(abs(corr_out_all),axis=0)
-    #print conv_num(i_name),float(np.count_nonzero(corr_out>0.5))/float(corr_out.size)
-    #print conv_num(i_name),np.count_nonzero(corr_out>0.1)
-  plt.show()
 
-#np.savetxt("phi_corr_"+i_name+".csv",corr_out,delimiter=",")
-#output of phi function
-#sprintf(out_name,"./jiken/%s/phi_out.csv",argv[1]);
-#sprintf(phi_data,"./jiken/%s/phi_stdata.csv",argv[1]);//statistical data
+  x = np.array(range(1,USR_NUM+1))
+  plt.bar(x-0.25,sim[:,0],color="pink",width=0.25,label="happy")
+  plt.bar(x,sim[:,1],color="orange",width=0.25,label="surprise")
+  plt.bar(x+0.25,sim[:,2],color="red",width=0.25,label="angry")
+  plt.bar(x+0.5,sim[:,3],color="blue",width=0.25,label="sad")
+  plt.legend()
+  plt.xlabel("usr id")
+  plt.ylabel("correlation between function accuracy C and weight W")
+  plt.savefig("weight_vs_acu.eps")
+  #plt.show()
