f432da2c1f6a1bac725509027e3344f321d2fa31
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2020-02-03 03:05:44 +0900

    add graph to output gosa and corr

---
+++ 40_compare_corr2.py
@@ -0,0 +1,88 @@
+import numpy as np
+import scipy as sp
+from scipy.stats import pearsonr
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from scipy.stats import iqr
+
+
+test_len = 30
+
+hito_num = 9
+
+set_list = (5, 10, 15, 20, 25, 30,35,40)
+
+nn_ave = np.zeros((len(set_list),hito_num))
+phi_ave = np.zeros((len(set_list),hito_num))
+
+nn_all = np.zeros((len(set_list),hito_num,test_len))
+phi_all = np.zeros((len(set_list),hito_num,test_len))
+
+phi_minmax = np.zeros((2,len(set_list),hito_num))
+
+sum_ave = 0
+sum_ave2 = 0
+sum_ave3 = 0
+
+for name_num in range(hito_num):
+  dir_name = "./jrm_test/" + str(name_num+1)
+
+  for set_num in set_list:
+
+    nn_corr = np.loadtxt(dir_name + "/nn_corr-" + str(set_num) + ".csv", delimiter=",")
+    phi_corr = np.loadtxt(dir_name + "/phi_corr-" + str(set_num) + ".csv", delimiter=",")
+
+    #phi_corr[np.isinf(nn_corr)] = 0
+    #nn_corr[np.isnan(nn_corr)] = 0
+
+    nn_all[set_num/5-1,name_num,:]= nn_corr
+    phi_all[set_num/5-1,name_num,:]= phi_corr
+
+    nn_ave[set_num/5-1,name_num]= np.average(nn_corr[nn_corr>0])
+    phi_ave[set_num/5-1,name_num]= np.average(phi_corr[phi_corr>0])
+
+    left = np.arange(30)
+    width = 0.3
+
+
+for i in range(len(set_list)):
+  val = phi_all[i,:,:]
+
+  val = nn_all[i,:,:]
+
+columns = ['phi','nn']
+index = np.zeros((len(set_list),1))
+index[:,0] = np.linspace(5,40,len(set_list),dtype='int')
+
+df_phi_in = pd.DataFrame(data=index,columns=["the number of supervisor data"],dtype='int')
+df_nn_way = pd.DataFrame({'way':['neural network','neural network','neural network','neural network','neural network','neural network','neural network','neural network']},columns=["way"],dtype='object',index=[0,1,2,3,4,5,6,7])
+df_phi_way = pd.DataFrame({'way':['proposed method','proposed method','proposed method','proposed method','proposed method','proposed method','proposed method','proposed method']},columns=["way"],dtype='object',index=[0,1,2,3,4,5,6,7])
+df_nn_data = pd.DataFrame(data=nn_ave,columns=["0","1","2","3","4","5","6","7","8"],dtype='float',index=[0,1,2,3,4,5,6,7])
+df_phi_data = pd.DataFrame(data=phi_ave,columns=["0","1","2","3","4","5","6","7","8"],dtype='float',index=[0,1,2,3,4,5,6,7])
+
+df_nn = pd.concat([df_phi_in,df_nn_way,df_nn_data],axis=1)
+print "df_nn",df_nn
+
+df_phi = pd.concat([df_phi_in,df_phi_way,df_phi_data],axis=1)
+print "df_phi",df_phi
+
+
+phi_melt = pd.melt(df_phi, id_vars=['the number of supervisor data','way'],var_name='hito')
+nn_melt = pd.melt(df_nn, id_vars=['the number of supervisor data','way'],var_name='hito')
+
+data = pd.concat([nn_melt,phi_melt])
+data_new = data.rename(columns={'value':'correlation between estimated mood and self-assessed mood'})
+
+plt.figure(figsize=(8,6))
+sns.set(style='whitegrid',palette='bright')
+sns.set_context(font_scale=10)
+sns_plot = sns.pointplot(x='the number of supervisor data',y='correlation between estimated mood and self-assessed mood',hue='way',data=data_new,capsize=.2)
+
+#plt.ylim([0,0.3])
+
+fig = sns_plot.get_figure()
+plt.show()
+#plt.savefig('5-40diff_matome.eps')
---
+++ 40_compare_gosa_box.py
@@ -0,0 +1,61 @@
+import numpy as np
+import scipy as sp
+from scipy.stats import pearsonr
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+flatui = ["#9b59b6","#3498db"]
+graph_label = "estimation method"
+
+test_len = 30
+
+hito_num = 9
+
+nn_all = np.zeros((test_len,hito_num))
+phi_all = np.zeros((test_len,hito_num))
+
+for name_num in range(hito_num):
+  dir_name = "./jrm_test/" + str(name_num+1)
+  #dir_name2 = "./jrm_test/third_test/" + str(name_num+1)
+
+  set_num = 40
+
+  nn_corr = np.loadtxt(dir_name + "/nn_diff-" + str(set_num) + ".csv", delimiter=",")
+  phi_corr = np.loadtxt(dir_name + "/phi_diff-" + str(set_num) + ".csv", delimiter=",")
+
+  nn_all[:,name_num]= abs(nn_corr)
+  phi_all[:,name_num]= abs(phi_corr)
+
+columns = ['phi','nn']
+index = np.zeros((30,1))
+index[:,0] = np.linspace(0,29,30,dtype='int')
+
+df_phi_in = pd.DataFrame(data=index,columns=["test no"],dtype='int')
+df_phi_way = pd.DataFrame({graph_label:"proposed method"},columns=[graph_label],dtype='object',index=np.linspace(0,29,30,dtype='int'))
+df_phi_data = pd.DataFrame(data=phi_all,dtype='float',columns=["1","2","3","4","5","6","7","8","9"])
+df_phi = pd.concat([df_phi_way,df_phi_data],axis=1)
+
+df_nn_in = pd.DataFrame(data=index,columns=["test no"],dtype='int')
+df_nn_way = pd.DataFrame({graph_label:"neural network"},columns=[graph_label],dtype='object',index=np.linspace(0,29,30,dtype='int'))
+df_nn_data = pd.DataFrame(data=nn_all,dtype='float',columns=["1","2","3","4","5","6","7","8","9"])
+df_nn = pd.concat([df_nn_way,df_nn_data],axis=1)
+
+phi_melt = pd.melt(df_phi, id_vars=[graph_label])
+nn_melt = pd.melt(df_nn, id_vars=[graph_label])
+
+
+
+data= pd.concat([nn_melt,phi_melt])
+data2= data.rename(columns={'variable':'user id'})
+data_new = data2.rename(columns={'value':'absolute value of errors between self-assessed mood and estimated mood'})
+
+plt.figure(figsize=(8,6))
+sns.set_context(font_scale=10)
+sns_plot = sns.barplot(x='user id',y='absolute value of errors between self-assessed mood and estimated mood',hue=graph_label,data=data_new,capsize=.2)
+#sns_plot = sns.boxplot(x='user id',y='absolute value of errors between self-assessed mood and estimated mood',hue=graph_label,data=data_new)
+fig = sns_plot.get_figure()
+
+plt.show()
+#fig.savefig('vsnn_box.eps')
