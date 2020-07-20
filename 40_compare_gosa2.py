6155d9ffaa86b425eaeeb9870eed31cb5b22b1ae
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2020-07-20 23:08:08 +0900

    changed to check abstract gosa

--- 40_compare_gosa2.py
+++ 40_compare_gosa2.py
@@ -9,11 +9,10 @@ import seaborn as sns
 from scipy.stats import iqr
 
 
-test_len = 30
-
+test_len = 29
 hito_num = 9
 
-set_list = (5, 10, 15, 20, 25, 30,35,40)
+set_list = (25,26,27,28,29)#, 30,35,40)
 
 nn_ave = np.zeros((len(set_list),hito_num))
 phi_ave = np.zeros((len(set_list),hito_num))
@@ -22,7 +21,6 @@ nn_all = np.zeros((len(set_list),hito_num,test_len))
 phi_all = np.zeros((len(set_list),hito_num,test_len))
 
 phi_minmax = np.zeros((2,len(set_list),hito_num))
-print phi_minmax
 
 sum_ave = 0
 sum_ave2 = 0
@@ -34,34 +32,42 @@ for name_num in range(hito_num):
   for set_num in set_list:
 
     nn_corr = np.loadtxt(dir_name + "/nn_diff-" + str(set_num) + ".csv", delimiter=",")
-
     phi_corr = np.loadtxt(dir_name + "/phi_diff-" + str(set_num) + ".csv", delimiter=",")
+    #nn_corr = np.loadtxt(dir_name + "/nn_diff-" + str(set_num) + ".csv", delimiter=",")
+    #phi_corr = np.loadtxt(dir_name + "/phi_diff-" + str(set_num) + ".csv", delimiter=",")
 
-    nn_all[set_num/5-1,name_num,:]= nn_corr
-    phi_all[set_num/5-1,name_num,:]= phi_corr
+    print(nn_corr)
+    print(phi_corr)
 
-    nn_ave[set_num/5-1,name_num]= np.average((abs(nn_corr)))
-    phi_ave[set_num/5-1,name_num]= np.average((abs(phi_corr)))
+    phi_corr[np.isinf(nn_corr)] = 0
+    phi_corr[np.isinf(phi_corr)] = 0
+    phi_corr[np.isnan(nn_corr)] = 0
+    phi_corr[np.isnan(phi_corr)] = 0
+    nn_corr[np.isinf(phi_corr)] = 0
+    nn_corr[np.isinf(nn_corr)] = 0
+    nn_corr[np.isnan(phi_corr)] = 0
+    nn_corr[np.isnan(nn_corr)] = 0
 
-    left = np.arange(30)
-    width = 0.3
 
+    nn_all[set_num-25,name_num,:]= nn_corr
+    phi_all[set_num-25,name_num,:]= phi_corr
 
+    nn_ave[set_num-25,name_num]= np.average(abs(nn_corr))
+    phi_ave[set_num-25,name_num]= np.average(abs(phi_corr))
 
 for i in range(len(set_list)):
   val = phi_all[i,:,:]
-
   val = nn_all[i,:,:]
 
 columns = ['phi','nn']
 index = np.zeros((len(set_list),1))
-index[:,0] = np.linspace(5,40,len(set_list),dtype='int')
+index[:,0] = np.linspace(25,29,len(set_list),dtype='int')
 
 df_phi_in = pd.DataFrame(data=index,columns=["the number of supervisor data"],dtype='int')
-df_nn_way = pd.DataFrame({'way':['neural network','neural network','neural network','neural network','neural network','neural network','neural network','neural network']},columns=["way"],dtype='object',index=[0,1,2,3,4,5,6,7])
-df_phi_way = pd.DataFrame({'way':['proposed method','proposed method','proposed method','proposed method','proposed method','proposed method','proposed method','proposed method']},columns=["way"],dtype='object',index=[0,1,2,3,4,5,6,7])
-df_nn_data = pd.DataFrame(data=nn_ave,columns=["0","1","2","3","4","5","6","7","8"],dtype='float',index=[0,1,2,3,4,5,6,7])
-df_phi_data = pd.DataFrame(data=phi_ave,columns=["0","1","2","3","4","5","6","7","8"],dtype='float',index=[0,1,2,3,4,5,6,7])
+df_nn_way = pd.DataFrame({'way':['neural network','neural network','neural network','neural network','neural network']},columns=["way"],dtype='object',index=[0,1,2,3,4])
+df_phi_way = pd.DataFrame({'way':['proposed method','proposed method','proposed method','proposed method','proposed method']},columns=["way"],dtype='object',index=[0,1,2,3,4])
+df_nn_data = pd.DataFrame(data=nn_ave,columns=["0","1","2","3","4","5","6","7","8"],dtype='float',index=[0,1,2,3,4])
+df_phi_data = pd.DataFrame(data=phi_ave,columns=["0","1","2","3","4","5","6","7","8"],dtype='float',index=[0,1,2,3,4])
 
 df_nn = pd.concat([df_phi_in,df_nn_way,df_nn_data],axis=1)
 print df_nn
@@ -79,9 +85,12 @@ data_new = data.rename(columns={'value':'difference between estimated mood and s
 plt.figure(figsize=(8,6))
 sns.set(style='whitegrid',palette='bright')
 sns.set_context(font_scale=10)
-#sns_plot = sns.barplot(x='the number of supervisor data',y='difference between estimated mood and self-assessed mood',hue='way',data=data_new,capsize=.2)
+sns_plot = sns.barplot(x='the number of supervisor data',y='difference between estimated mood and self-assessed mood',hue='way',data=data_new,capsize=.2)
+
+#sns_plot = sns.violinplot(x='the number of supervisor data',y='difference between estimated mood and self-assessed mood',hue='way',data=data_new,capsize=.2)
+
 #sns_plot = sns.boxplot(x='the number of supervisor data',y='difference between estimated mood and self-assessed mood',hue='way',data=data_new)
-sns_plot = sns.pointplot(x='the number of supervisor data',y='difference between estimated mood and self-assessed mood',hue='way',data=data_new,capsize=.2)
+#sns_plot = sns.pointplot(x='the number of supervisor data',y='difference between estimated mood and self-assessed mood',hue='way',data=data_new,capsize=.2)
 
 #plt.ylim([0,0.3])
 
