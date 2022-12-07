890ef11673c8ad3faa1458cc7bca92141e1faf81
kaz-mintan <kumagai@mizuuchi.lab.tuat.ac.jp>
2022-12-07 17:58:56 +0900

    updated to output graph of f vs s for each user

--- kitekan_make/fitting.py
+++ kitekan_make/fitting.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy import interpolate
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def get_spl(x_data,y_data):
   f_sci = interpolate.interp1d(x_data,y_data,kind="cubic")
@@ -11,24 +12,69 @@ FACTOR_NUM = 10
 FACE_TYPE = 4
 
 DIR_PATH = "../../est_pred_pc/data/"
+face_type_list=["happy","surprised","angry","sad"]
+factor_type_list=["trial num","rate of win","rate of encourage behavior","rate of sympathetic behavior", "rate of teasing behavior","rate of un-related behavior","rate of no behavior","total point","consecutive wins","consecutive loses"]
 
-def show_graph(factor_data,signal_data):
+#def ret_data(x_data,y_data,mental_data):
+def ret_data(df):
+  ##sorted x and y
+  x_data=np.array(df["factor"])+0.0001
+  y_data=np.array(df["signal"])
+  mental_data=np.array(df["mental"])
+
+  x_data_2=x_data[np.argsort(x_data)]
+  y_data_2=y_data[np.argsort(x_data)]
+  mental_2=mental_data[np.argsort(x_data)]
+
+  res = np.polyfit(x_data_2,y_data_2,2)
+  y_res=np.poly1d(res)(x_data_2)
+  return x_data_2,y_res
+
+def show_graph(username,factor_data,signal_data,mental_data):
   for factor_type in range(FACTOR_NUM):
     for signal_type in range(FACE_TYPE):
-      for m in range(10):
-        x_data = factor_data[:,factor_type]
-        y_data = signal_data[:,signal_type]
-        f_sci = interpolate.interp1d(x_data,y_data,kind="cubic")
-        plt.plot(x_data,y_data,'o')
-        plt.plot(x_data,f_sci(x_data),'-')
-        plt.show()
+      #for m in range(10):
+      x_data = factor_data[:,factor_type]
+      y_data = signal_data[:,signal_type]
+
+      df=pd.DataFrame(x_data,columns=["factor"])
+      df["signal"]=y_data
+      df["mental"]=mental_data
+
+      df_sml=df.query('0.4>mental>=0')
+      val_sml=int(df_sml["factor"].count())
+      df_mid=df.query('0.7>mental>=0.4')
+      val_mid=int(df_mid["factor"].count())
+      df_big=df.query('1>=mental>=0.7')
+      val_big=int(df_big["factor"].count())
+
+      if(val_sml*val_mid*val_big>0):
+        x_sml,y_res_sml=ret_data(df_sml)
+        x_mid,y_res_mid=ret_data(df_mid)
+        x_big,y_res_big=ret_data(df_big)
+
+        plt.plot(x_sml,y_res_sml,color='blue',label="sml",linestyle="dotted")
+        plt.plot(x_mid,y_res_mid,color='green',label="mid",linestyle="dashed")
+        plt.plot(x_big,y_res_big,color='red',label="big",linestyle="solid")
+      plt.scatter(x_data,y_data,c=mental_data)
+      plt.legend()
+
+      plt.ylabel("face data("+face_type_list[signal_type]+")")
+      plt.xlabel("factor data("+factor_type_list[factor_type]+")")
+
+      filename="./plot/graph_u"+str(username)+"_f"+str(factor_type)+"_s"+str(signal_type)+".png"
+      plt.savefig(filename)
+      print("u",username,"f",factor_type,"s",signal_type)
+      plt.clf()
 
 
-for username in range(1,USER_NUM):
+for username in range(1,USER_NUM+1):
   factor_file = DIR_PATH+str(username)+"/factor_before.csv"
   signal_file = DIR_PATH+str(username)+"/signal_before.csv"
+  mental_file = DIR_PATH+str(username)+"/kibun_before.csv"
 
   factor_data = np.loadtxt(factor_file,delimiter="\t")
   signal_data = np.loadtxt(signal_file,delimiter="\t")
+  mental_data = np.loadtxt(mental_file,delimiter="\t")
 
-  show_graph(factor_data,signal_data)
+  show_graph(username,factor_data,signal_data,mental_data)
