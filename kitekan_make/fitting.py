3e21386971873a4d9f5bd01364a348a7bff09e71
kaz-mintan <kumagai@mizuuchi.lab.tuat.ac.jp>
2022-12-12 11:00:15 +0900

    kitekan hybrid maker

--- kitekan_make/fitting.py
+++ kitekan_make/fitting.py
@@ -3,6 +3,9 @@ from scipy import interpolate
 import matplotlib.pyplot as plt
 import pandas as pd
 
+print("input mode all/each")
+mode=input()
+
 def get_spl(x_data,y_data):
   f_sci = interpolate.interp1d(x_data,y_data,kind="cubic")
 
@@ -13,6 +16,21 @@ FACE_TYPE = 4
 DIR_PATH = "../../est_pred_pc/data/"
 face_type_list=["happy","surprised","angry","sad"]
 factor_type_list=["trial num","rate of win","rate of encourage behavior","rate of sympathetic behavior", "rate of teasing behavior","rate of un-related behavior","rate of no behavior","total point","consecutive wins","consecutive loses"]
+
+def ret_data_all(df,factor_type,signal_type):
+
+  x_data=np.array(df[factor_type_list[factor_type]])+0.0001
+  y_data=np.array(df[face_type_list[signal_type]])
+  mental_data=np.array(df["mental"])
+
+  x_data_2=x_data[np.argsort(x_data)]
+  y_data_2=y_data[np.argsort(x_data)]
+  mental_2=mental_data[np.argsort(x_data)]
+
+  res = np.polyfit(x_data_2,y_data_2,2)
+  y_res=np.poly1d(res)(x_data_2)
+  return x_data_2,y_res
+
 
 #def ret_data(x_data,y_data,mental_data):
 def ret_data(df):
@@ -27,49 +45,55 @@ def ret_data(df):
 
   res = np.polyfit(x_data_2,y_data_2,2)
   y_res=np.poly1d(res)(x_data_2)
-  return x_data_2,y_res
+  return res,x_data_2,y_res
 
 def show_graph(username,factor_data,signal_data,mental_data,thr):
-  for factor_type in range(FACTOR_NUM):
-    for signal_type in range(FACE_TYPE):
-      #for m in range(10):
-      x_data = factor_data[:,factor_type]
-      y_data = signal_data[:,signal_type]
+  with open("res_each.csv",'a') as f:
+    for factor_type in range(FACTOR_NUM):
+      for signal_type in range(FACE_TYPE):
+        #for m in range(10):
+        x_data = factor_data[:,factor_type]
+        y_data = signal_data[:,signal_type]
 
-      df=pd.DataFrame(x_data,columns=["factor"])
-      df["signal"]=y_data
-      df["mental"]=mental_data
+        df=pd.DataFrame(x_data,columns=["factor"])
+        df["signal"]=y_data
+        df["mental"]=mental_data
 
-      df_sml=df.query(str(thr[1])+'>mental>='+str(thr[0]))
-      val_sml=int(df_sml["factor"].count())
-      df_mid=df.query(str(thr[2])+'>mental>='+str(thr[1]))
-      val_mid=int(df_mid["factor"].count())
-      df_big=df.query(str(thr[3])+'>=mental>='+str(thr[2]))
-      val_big=int(df_big["factor"].count())
+        df_sml=df.query(str(thr[1])+'>mental>='+str(thr[0]))
+        val_sml=int(df_sml["factor"].count())
+        df_mid=df.query(str(thr[2])+'>mental>='+str(thr[1]))
+        val_mid=int(df_mid["factor"].count())
+        df_big=df.query(str(thr[3])+'>=mental>='+str(thr[2]))
+        val_big=int(df_big["factor"].count())
 
-      if(val_sml*val_mid*val_big>0):
-        x_sml,y_res_sml=ret_data(df_sml)
-        x_mid,y_res_mid=ret_data(df_mid)
-        x_big,y_res_big=ret_data(df_big)
+        if(val_sml*val_mid*val_big>0):
+          ret_sml,x_sml,y_res_sml=ret_data(df_sml)
+          ret_mid,x_mid,y_res_mid=ret_data(df_mid)
+          ret_big,x_big,y_res_big=ret_data(df_big)
+          print(username,factor_type,signal_type,ret_sml,ret_mid,ret_big,file=f)
 
-        plt.plot(x_sml,y_res_sml,color='blue',label="sml",linestyle="dotted")
-        plt.plot(x_mid,y_res_mid,color='green',label="mid",linestyle="dashed")
-        plt.plot(x_big,y_res_big,color='red',label="big",linestyle="solid")
-      plt.scatter(x_data,y_data,c=mental_data)
-      plt.legend()
+          plt.plot(x_sml,y_res_sml,color='blue',label="sml",linestyle="dotted")
+          plt.plot(x_mid,y_res_mid,color='green',label="mid",linestyle="dashed")
+          plt.plot(x_big,y_res_big,color='red',label="big",linestyle="solid")
+        plt.scatter(x_data,y_data,c=mental_data)
+        plt.legend()
 
-      plt.ylabel("face data("+face_type_list[signal_type]+")")
-      plt.xlabel("factor data("+factor_type_list[factor_type]+")")
+        plt.ylabel("face data("+face_type_list[signal_type]+")")
+        plt.xlabel("factor data("+factor_type_list[factor_type]+")")
 
-      filename="./plot/graph_u"+str(username)+"_f"+str(factor_type)+"_s"+str(signal_type)+".png"
-      plt.savefig(filename)
-      print("u",username,"f",factor_type,"s",signal_type)
-      plt.clf()
+        filename="./plot/graph_u"+str(username)+"_f"+str(factor_type)+"_s"+str(signal_type)+".png"
+        #plt.savefig(filename)
+        print("u",username,"f",factor_type,"s",signal_type)
+        plt.clf()
 
+if(mode=="each"):
+  filename='mental_thr_memo.csv'
+  data=np.loadtxt(filename,delimiter=",")
 
-with open('mental_thr_memo.csv', 'a') as f:
   for username in range(1,USER_NUM+1):
     thr=[0,0.4,0.7,1]
+    thr[1]=data[username-1,1]
+    thr[2]=data[username-1,2]
 
     factor_file = DIR_PATH+str(username)+"/factor_before.csv"
     signal_file = DIR_PATH+str(username)+"/signal_before.csv"
@@ -80,25 +104,117 @@ with open('mental_thr_memo.csv', 'a') as f:
     mental_data = np.loadtxt(mental_file,delimiter="\t")
     df_mental = pd.DataFrame(mental_data,columns=["mental"])
 
-    for trial in range(2):
-      plt.hist(df_mental["mental"])
-      #plt.vlines([df_mental.describe()["mental"]["mean"]], 0, 30, "blue", linestyles='dashed')
-      plt.vlines(thr[1], 0, 30, "red", linestyles='dashed')
-      plt.vlines(thr[2], 0, 30, "green", linestyles='dashed')
-      plt.show()
-      for i in range(2):
-        print("thr["+str(i+1)+"]?")
-        #if(input() is not ""):
-        #  thr[1]=input()
-        val=input()
-        if(val is not ""):
-          print("val:",val)
-          thr[i+1]=float(val)
-        else:
-          print("noval")
-      print(thr)
-
-    input()
-    #print(str(username)+":"+str(thr[1])+","+str(thr[2])+"\n", file=f)
-
     show_graph(username,factor_data,signal_data,mental_data,thr)
+
+elif(mode=="set"):
+  with open('mental_thr_memo.csv', 'a') as f:
+    for username in range(1,USER_NUM+1):
+      thr=[0,0.4,0.7,1]
+
+      factor_file = DIR_PATH+str(username)+"/factor_before.csv"
+      signal_file = DIR_PATH+str(username)+"/signal_before.csv"
+      mental_file = DIR_PATH+str(username)+"/kibun_before.csv"
+
+      factor_data = np.loadtxt(factor_file,delimiter="\t")
+      signal_data = np.loadtxt(signal_file,delimiter="\t")
+      mental_data = np.loadtxt(mental_file,delimiter="\t")
+      df_mental = pd.DataFrame(mental_data,columns=["mental"])
+
+      for trial in range(2):
+        plt.hist(df_mental["mental"])
+        #plt.vlines([df_mental.describe()["mental"]["mean"]], 0, 30, "blue", linestyles='dashed')
+        plt.vlines(thr[1], 0, 30, "red", linestyles='dashed')
+        plt.vlines(thr[2], 0, 30, "green", linestyles='dashed')
+        plt.show()
+        for i in range(2):
+          print("thr["+str(i+1)+"]?")
+          #if(input() is not ""):
+          #  thr[1]=input()
+          val=input()
+          if(val is not ""):
+            print("val:",val)
+            thr[i+1]=float(val)
+          else:
+            print("noval")
+        print(thr)
+
+      input()
+      #print(str(username)+":"+str(thr[1])+","+str(thr[2])+"\n", file=f)
+
+      show_graph(username,factor_data,signal_data,mental_data,thr)
+else:
+
+  filename='mental_thr_memo.csv'
+  data=np.loadtxt(filename,delimiter=",")
+
+  #for factor_type in range(FACTOR_NUM):
+  #  for signal_type in range(FACE_TYPE):
+
+  for username in range(1,USER_NUM+1):
+    thr=[0,0.4,0.7,1]
+    thr[1]=data[username-1,1]
+    thr[2]=data[username-1,2]
+
+    factor_file = DIR_PATH+str(username)+"/factor_before.csv"
+    signal_file = DIR_PATH+str(username)+"/signal_before.csv"
+    mental_file = DIR_PATH+str(username)+"/kibun_before.csv"
+
+    factor_data = np.loadtxt(factor_file,delimiter="\t")
+    signal_data = np.loadtxt(signal_file,delimiter="\t")
+    mental_data = np.loadtxt(mental_file,delimiter="\t")
+
+    df_f=pd.DataFrame(factor_data,columns=factor_type_list)
+    df_s=pd.DataFrame(signal_data,columns=face_type_list)
+    df=pd.concat([df_f,df_s],axis=1)
+    df["mental"]=mental_data
+
+    if(username==1):
+      df_sml=df.query(str(thr[1])+'>mental>='+str(thr[0]))
+      df_mid=df.query(str(thr[2])+'>mental>='+str(thr[1]))
+      df_big=df.query(str(thr[3])+'>=mental>='+str(thr[2]))
+    else:
+      _sml=df.query(str(thr[1])+'>mental>='+str(thr[0]))
+      _mid=df.query(str(thr[2])+'>mental>='+str(thr[1]))
+      _big=df.query(str(thr[3])+'>=mental>='+str(thr[2]))
+      df_sml=df_sml.append(_sml)
+      df_mid=df_mid.append(_mid)
+      df_big=df_big.append(_big)
+
+  for factor_type in range(FACTOR_NUM):
+    for signal_type in range(FACE_TYPE):
+
+      x_sml,y_res_sml=ret_data_all(df_sml,factor_type,signal_type)
+      x_mid,y_res_mid=ret_data_all(df_mid,factor_type,signal_type)
+      x_big,y_res_big=ret_data_all(df_big,factor_type,signal_type)
+
+      print("x_sml",x_sml)
+      print("y_res_sml",y_res_sml)
+
+      plt.plot(x_sml,y_res_sml,color='blue',label="sml",linestyle="dotted")
+      plt.plot(x_mid,y_res_mid,color='green',label="mid",linestyle="dashed")
+      plt.plot(x_big,y_res_big,color='red',label="big",linestyle="solid")
+
+      x_data_sml=df_sml[factor_type_list[factor_type]]
+      y_data_sml=df_sml[face_type_list[signal_type]]
+      plt.scatter(x_data_sml,y_data_sml,color="blue",alpha=0.5)
+
+      x_data_mid=df_mid[factor_type_list[factor_type]]
+      y_data_mid=df_mid[face_type_list[signal_type]]
+      plt.scatter(x_data_mid,y_data_mid,color="green",alpha=0.5)
+
+      x_data_big=df_big[factor_type_list[factor_type]]
+      y_data_big=df_big[face_type_list[signal_type]]
+      plt.scatter(x_data_big,y_data_big,color="red",alpha=0.5)
+
+      plt.legend()
+
+      plt.ylabel("face data("+face_type_list[signal_type]+")")
+      plt.xlabel("factor data("+factor_type_list[factor_type]+")")
+
+      filename="./plot/graph_u"+str(username)+"_f"+str(factor_type)+"_s"+str(signal_type)+".png"
+      plt.show()
+      #plt.savefig(filename)
+      print("u",username,"f",factor_type,"s",signal_type)
+      plt.clf()
+
+
---
+++ kitekan_make/show_kitekan.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+
+USER_NUM = 9
+FACTOR_NUM = 10
+FACE_TYPE = 4
+
+print("input plot directory name(without any /)")
+dirname=input()
+
+test_im=cv2.imread("./"+dirname+"/graph_u2_f4_s3.png")
+height,width=test_im.shape[:2]
+#_im=np.zeros_like(test_im)
+#im=np.tile(_im,9)
+#print(im.shape)
+#input()
+print("input show graph or save graph: save/show")
+flg=input()
+
+for factor_type in range(FACTOR_NUM):
+  for signal_type in range(FACE_TYPE):
+
+    im1=cv2.imread("./"+dirname+"/graph_u1_f"+str(factor_type)+"_s"+str(signal_type)+".png")
+    im2=cv2.imread("./"+dirname+"/graph_u2_f"+str(factor_type)+"_s"+str(signal_type)+".png")
+    im3=cv2.imread("./"+dirname+"/graph_u3_f"+str(factor_type)+"_s"+str(signal_type)+".png")
+    im4=cv2.imread("./"+dirname+"/graph_u4_f"+str(factor_type)+"_s"+str(signal_type)+".png")
+    im5=cv2.imread("./"+dirname+"/graph_u5_f"+str(factor_type)+"_s"+str(signal_type)+".png")
+    im6=cv2.imread("./"+dirname+"/graph_u6_f"+str(factor_type)+"_s"+str(signal_type)+".png")
+    im7=cv2.imread("./"+dirname+"/graph_u7_f"+str(factor_type)+"_s"+str(signal_type)+".png")
+    im8=cv2.imread("./"+dirname+"/graph_u8_f"+str(factor_type)+"_s"+str(signal_type)+".png")
+    im9=cv2.imread("./"+dirname+"/graph_u9_f"+str(factor_type)+"_s"+str(signal_type)+".png")
+
+    im_h1=cv2.hconcat([im1,im2,im3])
+    im_h2=cv2.hconcat([im4,im5,im6])
+    im_h3=cv2.hconcat([im7,im8,im9])
+    im_all = cv2.vconcat([im_h1,im_h2,im_h3])
+    save_fig=cv2.resize(im_all,dsize=(width*2,height*2))
+    title="f"+str(factor_type)+"_s"+str(signal_type)
+
+    if(flg=="save"):
+      savefig_filename="./"+dirname+"/total/"+title+".png"
+      cv2.imwrite(savefig_filename,save_fig)
+    elif(flg=="show"):
+      cv2.imshow(title,save_fig)
+      cv2.waitKey(0)
+      cv2.destroyWindow(title)
